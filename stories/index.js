3dcd3130e560c5c397433f1f832ca2610d8b49e8
sabazusi <kouhei.live@gmail.com>
2017-04-10 23:21:54 +0900

    Add storybook

---
+++ stories/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { storiesOf, action } from '@kadira/storybook';
+import ColorPicker from '../src/index';
+
+storiesOf('ColorPicker', module)
+  .add('default: #ff0000', () => (
+    <ColorPicker />
+  ));
