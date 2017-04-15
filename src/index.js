5c795d0101e0ec349dba52f41ad222fe03f80bf5
sabazusi <kouhei.live@gmail.com>
2017-04-15 18:55:39 +0900

    Show current color with clicking pallet

--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
 // @flow
 
 import React from 'react';
+import Color from 'color';
 
 type Props = {
   defaultColor: string;
 };
 
 type State = {
-  current: string;
+  hsv: {
+    h: number;
+    s: number;
+    v: number;
+  }
 };
 
 export default class ReactColorPicker extends React.Component<void, Props, State> {
@@ -15,7 +20,11 @@ export default class ReactColorPicker extends React.Component<void, Props, State
   constructor() {
     super();
     this.state = {
-      current: 'ff0000'
+      hsv: {
+        h: 0,
+        s: 0,
+        v: 0
+      }
     };
   }
 
@@ -25,31 +34,47 @@ export default class ReactColorPicker extends React.Component<void, Props, State
   }
 
   onClickPallet = (e: Event) => {
-    console.log(e.nativeEvent.offsetX);
-    console.log(e.nativeEvent.offsetY);
+    const hsv = {
+      s: parseInt(100 * e.nativeEvent.offsetX / 300, 10),
+      v: parseInt(100 * (300 - e.nativeEvent.offsetY) / 300, 10)
+    }
+    this.setState({
+      hsv: Object.assign({}, this.state.hsv, hsv)
+    });
   }
 
   render() {
+    const {hsv} = this.state;
+    const color = Color(hsv).hsl().color;
     return (
-      <div
-        onClick={this.onClickPallet}
-        style={{
-          cursor: 'pointer',
-          width: 300,
-          height: 300,
-          backgroundColor: '#f00'
-        }
-      }>
-        <div style={{
-          width: 300,
-          height: 300,
-          background: 'linear-gradient(to right, #fff, rgba(204,154,129,0))'
-        }}>
+      <div>
+        <div
+          style={{
+            width: 50,
+            height: 50,
+            backgroundColor: `hsl(${color[0]}, ${color[1]}%, ${color[2]}%)`
+          }}
+        />
+        <div
+          onClick={this.onClickPallet}
+          style={{
+            cursor: 'pointer',
+            width: 300,
+            height: 300,
+            backgroundColor: '#f00'
+          }
+        }>
           <div style={{
             width: 300,
             height: 300,
-            background: 'linear-gradient(to top, #000, rgba(204,154,129,0))'
-          }} />
+            background: 'linear-gradient(to right, #fff, rgba(204,154,129,0))'
+          }}>
+            <div style={{
+              width: 300,
+              height: 300,
+              background: 'linear-gradient(to top, #000, rgba(204,154,129,0))'
+            }} />
+          </div>
         </div>
       </div>
     );
