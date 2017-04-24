cf2ccc2ebf73a7b731c874308b211b44c22ca2e9
sabazusi <kouhei.live@gmail.com>
2017-04-24 19:44:35 +0900

    Build files by webpack with css

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -1,5 +1,6 @@
 import webpack from 'webpack';
 import path from 'path';
+import ExtractTextPlugin from 'extract-text-webpack-plugin';
 
 export default {
   entry: path.join(__dirname, 'src', 'index.js'),
@@ -17,6 +18,21 @@ export default {
       {
         test: /\.js$/,
         use: 'babel-loader'
+      },
+      {
+        test: /\.css$/,
+        loader: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: [
+            {
+              loader: 'css-loader',
+              query: {
+                modules: true,
+                sourceMap: true
+              }
+            }
+          ]
+        })
       }
     ]
   }
